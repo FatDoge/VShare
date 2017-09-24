7e77a5e005e761fea68e3ba14cfc253e97fff9cc
熊吉 <704274491@qq.com>
2017-09-24 17:52:37 +0800

    add the database to check user info

--- js/login.js
+++ js/login.js
@@ -1,8 +1,35 @@
+var temp=false;
 function judge(){
+	var url="http://hduzjh.cn:8080/VSCOM/LoginServlet";
 	var username=$("#inputEmail3").val();
-	console.log(username);
 	var password=$("#inputPassword3").val();
-	console.log(password);
-	if(username!="i@admin.com"||password!="8888")
-	return false;
+	url+="?username="+username+"&password="+password;
+	$.getJSON(url, function(data) {
+    console.log(data);
+    console.log(typeof(data.status));
+    if(data.status) success();
+    else {
+    error();
+    return temp;
+    }
+    })
+    
 }
+function error(){
+	$("#warning").slideDown(700);
+    function hide(){
+    	$("#warning").slideUp(700);
+    }
+    setTimeout(hide,1200);
+}
+function success(){
+	$("#success").slideDown(700);
+    function hide(){
+    	$("#success").slideUp(700);
+    }
+    setTimeout(jump,900);
+    function jump(){
+    self.location='./selfcenter.html';
+    }
+}
+
