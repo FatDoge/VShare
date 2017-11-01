cc02fc9f706545970cb8d39d1709b970170fdaa9
熊吉 <704274491@qq.com>
2017-11-01 17:23:52 +0800

    add delete and add module

--- js/main.js
+++ js/main.js
@@ -10,7 +10,21 @@ setTimeout(showPic,1000);
 $("#showMore").hide(500);
 })
 // 过渡显示
+$("#input-id").fileinput({
+    'showUpload': true,
+    'previewFileType': 'any'
+});
 
+$("#closeInput").click(function () {
+    console.log("关闭点击");
+    $(".fullPage").slideUp();
+    $("#addPic").show();
+})
+$("#addPic").click(function(){
+    console.log("点击添加");
+    $("#addPic").hide();
+    $(".fullPage").slideDown();
+})
 	// 点赞
 $('body').on("click",'.heart',function()
     {
